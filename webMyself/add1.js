058b889d74346e27347584082809cc216f608cb2
魏川傅 <81062883+Chuanfu-Wei@users.noreply.github.com>
2021-03-21 23:22:58 +0800

    Create add1.js

---
+++ webMyself/add1.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+
+  function checkNum(nums) {
+                if (nums < 10) {
+                    return '0' + nums;
+                }
+                return nums;
+            }
+
+            var mydiv = document.querySelector('#examDate');
+            function showTime() {
+                var now = new Date();
+                var target = new Date(2022, 2, 20)
+                var result = Math.floor((target.getTime() - now.getTime()) / 1000); // 总秒数
+                if (result == 0) {
+                    mydiv.innerText = '今天考试啦！！！';
+                    return;
+                }
+                var days = checkNum(Math.floor(result / (24 * 60 * 60))); // 天数
+                var hours = checkNum(Math.floor((result / (60 * 60))) % 24); // 小时
+                var minutes = checkNum(Math.floor((result / 60) % 60)); // 分钟
+                var seconds = checkNum(Math.floor(result % 60)); // 秒数
+                mydiv.innerText = '距离2022年专转本考试还有：' + days + '天' + hours + '小时' + minutes + '分钟' + seconds + '秒';
+
+                setTimeout(showTime, 1000);
+            }
+            showTime();
+
+
+}
